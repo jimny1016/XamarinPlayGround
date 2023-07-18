232912891769febdd64905f4d5aad5bc03dc0113
Jimmy <jimmy.chu@ibuypower.com>
2023-07-18 15:05:28 +0800

    Adjust code style.

--- XamarinPlayground/MyApp1/MainActivity.cs
+++ XamarinPlayground/MyApp1/MainActivity.cs
@@ -10,13 +10,13 @@ using AndroidX.AppCompat.App;
 using Java.Lang.Reflect;
 using Java.Lang;
 using Newtonsoft.Json;
+using Java.IO;
 
 namespace MyApp1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btn;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,8 +24,11 @@ namespace MyApp1
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            btn = FindViewById<Button>(Resource.Id.big);
-            btn.Click += Button_Click;
+            var switchToRndisBtn = FindViewById<Button>(Resource.Id.SwitchToRndis);
+            switchToRndisBtn.Click += SwitchToRndis_Click;
+
+            var switchToAndroidControllModeBtn = FindViewById<Button>(Resource.Id.SwitchToAndroidControllMode);
+            switchToAndroidControllModeBtn.Click += SwitchToAndroidControllMode_Click;            
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -52,14 +55,13 @@ namespace MyApp1
             alertDialog.Show();
         }
 
-
-        private void Button_Click(object sender, System.EventArgs e)
+        private void SwitchToRndis_Click(object sender, EventArgs e)
         {
-            btn.Text = "Clicked";
-
-            var result = ToggleUSBTransferMode();
-            ShowAlertDialog(result.ToString());
-            
+            ShowAlertDialog($"ToggleUSBTransferMode:{ToggleUSBTransferMode()}");
+        }
+        private void SwitchToAndroidControllMode_Click(object sender, EventArgs e)
+        {
+            ShowAlertDialog($"SwitchToControlMode:{SwitchToControlMode()}");
         }
 
         public bool ToggleUSBTransferMode()
@@ -95,6 +97,24 @@ namespace MyApp1
             {
                 ShowAlertDialog(JsonConvert.SerializeObject(e));
                 e.PrintStackTrace();
+            }
+            return false;
+        }
+
+        public bool SwitchToControlMode() 
+        {
+            try
+            {
+                BufferedWriter fops_2 = new BufferedWriter(new FileWriter("/sys/class/vd_gpio/cust_enjie/vd_enjie_ext_power"));//open node
+                fops_2.Write("1");
+                fops_2.Close();
+
+                return true;
+            } 
+            catch (Java.Lang.Exception ex)
+            {
+                ShowAlertDialog(JsonConvert.SerializeObject(ex));
+                ex.PrintStackTrace();
             }
             return false;
         }
